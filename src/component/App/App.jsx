846d1c1563bcf6491f4e76e5b54733690cf20906
Yevhen-Karpov <goldblogart@gmail.com>
2021-12-15 14:30:09 +0200

    1

--- src/component/App/App.jsx
+++ src/component/App/App.jsx
@@ -5,12 +5,7 @@ import Form from '../Form/Form';
 import Filter from '../Filter/Filter';
 export default class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -51,7 +46,15 @@ export default class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
-
+  componentDidMount() {
+    const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+    this.setState({ contacts: parsedContacts });
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   render() {
     const { filter } = this.state;
     const visibleContacts = this.getVisibleContacts();
